be93efb2bb2a92b073c16b80b82deb9bccb2ad27
Bugstorm <s.w.de.jong@live.nl>
2015-03-11 09:38:22 +0100

    Create ActivationFunction.ino

---
+++ ActivationFunction.ino
@@ -0,0 +1,20 @@
+int incomingByte = 0;
+boolean activation;
+
+void setup() {
+  Serial.begin(9600);
+}
+boolean activationFunction(){
+  if (Serial.available() > 0){
+    incomingByte = Serial.read();
+    if (isAlpha(incomingByte)){
+      return true;
+    }
+  }
+}
+void loop() {
+  activation = activationFunction();
+  if (activation == true){
+    //stuff
+  }
+}
